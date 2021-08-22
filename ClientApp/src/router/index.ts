098d0fa6df181b5ca9befd2c9b7fb5c47a565558
BlinkyCrazyDev <fredericcazesulfourt@yahoo.fr>
2021-08-22 15:31:23 +0200

    First commit

--- ClientApp/src/router/index.ts
+++ ClientApp/src/router/index.ts
@@ -1,33 +1,29 @@
 import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
+import VueRouter from 'vue-router'
+import Home from '@/views/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes: Array<RouteConfig> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/counter',
-    name: 'counter',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "counter" */ '../views/Counter.vue')
-  },
-  {
-    path: '/fetch-data',
-    name: 'fetch-data',
-    component: () => import(/* webpackChunkName: "fetch-data" */ '../views/FetchData.vue')
-  }
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue')
+    }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
